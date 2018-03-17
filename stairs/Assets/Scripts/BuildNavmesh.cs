181edadc4d1320b0e10654fd7df790d79c4087fb
Thomas Stassin <thomas.stassin@gsmproductions.com>
2018-03-17 01:55:02 +0100

    dynamic navmesh

---
+++ stairs/Assets/Scripts/BuildNavmesh.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshSurface))]
+public class BuildNavmesh : MonoBehaviour {
+    public NavMeshSurface surface;
+    // Use this for initialization
+    void Start () {
+        surface = GetComponent<NavMeshSurface>();
+        }
+	
+	// Update is called once per frame
+	void Update () {
+
+        }
+}
---
+++ stairs/Assets/Scripts/ComeToMe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ComeToMe : MonoBehaviour {
+    NavMeshAgent agent;
+    public GameObject target;
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(target.transform.position);
+	}
+}
