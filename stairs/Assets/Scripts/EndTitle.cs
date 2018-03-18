42f7f22f56ad9d8373c61cf61deca0b91922931d
Renaud <repierrard@gmail.com>
2018-03-18 13:35:09 +0100

    gfdsg

---
+++ stairs/Assets/Scripts/EndTitle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTitle : MonoBehaviour {
+
+   public float count = 7;
+    public string LevelToLoad;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        count -= Time.deltaTime ;
+        if(count <= 0)
+        {
+            SceneManager.LoadScene(LevelToLoad);
+        }
+	}
+}
